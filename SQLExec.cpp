7a67872f718958e1cff60abd03dd080004c6aba4
Bruno Ruiz de Somocurcio <brunords@live.com>
2021-04-28 20:43:48 -0700

    SQLExec CreateTable Compile Errors Fixed commit #9.

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -83,6 +83,7 @@ QueryResult *SQLExec::create(const CreateStatement *statement) {
     if (statement->type != CreateStatement::kTable)
         throw SQLExecError("CREATE Statement Not recognized");
 
+    // Update _tables Schema
     string table_name = string(statement->tableName);
     ValueDict *table_entry = new ValueDict();
     Value val_table_name = Value(table_name);
@@ -90,6 +91,18 @@ QueryResult *SQLExec::create(const CreateStatement *statement) {
     tables->insert(table_entry); // May Throw DbRelationError
 
     try {
+        // Update _columns Schema
+        for (ColumnDefinition *col : *statement->columns) {
+            ValueDict *column_entry = new ValueDict();
+            Value val_table_name = Value(table_name);
+            Value val_column_name = string(col->name);
+            Value val_data_type = sting(if(col->type == ColumnDefinition::TEXT)? "TEXT" : "INT");
+            (*column_entry)["table_name"] = val_table_name;
+            (*column_entry)["column_name"] = val_column_name;
+            (*column_entry)["data_type"] = val_data_type;
+            tables->columns_table->insert(column_entry); // May Throw DbRelationError
+        }
+        // Update _columns Schema
         ColumnAttributes* column_attributes = new ColumnAttributes();
         ColumnNames *column_names = new ColumnNames();
         for (ColumnDefinition *col : *statement->columns) {
