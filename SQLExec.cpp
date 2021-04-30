ae13f902a38e307c978222c1e4548f353749eade
Bruno Ruiz de Somocurcio <brunords@live.com>
2021-04-30 12:50:46 -0700

    SQLExec.cpp creat() method update #4

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -70,9 +70,9 @@ void
 SQLExec::column_definition(const ColumnDefinition *col, Identifier &column_name, ColumnAttribute &column_attribute) {
     column_name = string(col->name);
     switch (col->type) {
-        case INT:
+        case ColumnDefinition::DataType::INT:
             column_attribute = ColumnAttribute(ColumnAttribute::INT);
-        case TEXT:
+        case ColumnDefinition::DataType::TEXT:
             column_attribute = ColumnAttribute(ColumnAttribute::TEXT);
         default:
             throw SQLExecError("Column Datatype not supported");
