71ba19c8e5081ca811e92346d3279155f21528e0
Bruno Ruiz de Somocurcio <44142527+BrunoRDS@users.noreply.github.com>
2021-05-06 16:05:34 -0700

    Displaying Columns from fixes.

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -210,7 +210,7 @@ QueryResult *SQLExec::show_columns(const ShowStatement *statement) {
         ValueDict* row = new ValueDict;
         (*row)["table_name"] = Value(statement->tableName);
         (*row)["column_name"] = Value(col_name);
-        (*row)["data_type"] = Value(column_attributes[i]->get_data_type());
+        (*row)["data_type"] = Value((*column_attributes)[i]->get_data_type());
         rows.push_back(row);
     }
 
