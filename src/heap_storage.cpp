ecb76f898b3f0fab3fbfd420a5d494eea41d595a
scrufulufugus <smonson@irbash.net>
2024-01-30 14:27:26 -0800

    Fix: db.stat writes a pointer
    db.stat writes a pointer to the passed in reference not the struct
    itself.

--- src/heap_storage.cpp
+++ src/heap_storage.cpp
@@ -224,12 +224,13 @@ void HeapFile::db_open(uint flags) {
   _DB_ENV->get_home(&dbname); // We dont need dbname so reuse it.
   this->dbfilename = std::string(dbname) + '/' + std::string(filename);
 
-  // FIXME: This still results in a massive last block for opening files
   // If the create flag is set then assume blank file.
   if ((flags & DB_CREATE) == 0U) {
-    DB_BTREE_STAT stat;
+    DB_BTREE_STAT *stat;
     this->db.stat(nullptr, &stat, DB_FAST_STAT);
-    this->last = stat.bt_ndata;
+    auto ndata = stat->bt_ndata;
+    free(stat);
+    this->last = ndata;
   } else {
     this->last = 0;
   }
